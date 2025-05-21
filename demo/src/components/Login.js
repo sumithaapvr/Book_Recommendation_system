d1080ca1d37eb45f982f2c8ad06687fd29df61ee
SUMITHAA P V R <sumithaa2004@gmail.com>
2025-05-21 13:10:56 +0530

    Update Login.js

--- demo/src/components/Login.js
+++ demo/src/components/Login.js
@@ -12,13 +12,14 @@ const Login = ({ onClose }) => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-      });
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
 
       const data = await response.json();
       if (data.token) {
