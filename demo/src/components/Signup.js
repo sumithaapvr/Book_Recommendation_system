fef368ead2dad8449ba7b17481066ca01aec67e5
SUMITHAA P V R <sumithaa2004@gmail.com>
2025-05-21 13:12:02 +0530

    Update Signup.js

--- demo/src/components/Signup.js
+++ demo/src/components/Signup.js
@@ -10,7 +10,7 @@ const Signup = ({ onClose }) => {
     e.preventDefault();
   
     try {
-      const response = await fetch('http://localhost:5000/api/auth/signup', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/signup`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
